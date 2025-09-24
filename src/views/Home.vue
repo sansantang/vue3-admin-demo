d297f8b90636fa7ccc693f5d89c7b316ced5ba70
ge.tang <tangge@vip.qq.com>
2025-09-24 16:21:39 +0800

    add chart

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -46,8 +46,9 @@
                     </div>
                 </div>
             </el-card>
-
-            <div id="chart" style="width: 600px; height: 400px;"></div>
+            <el-card>
+                <div id="chart" style=" height: 400px;"></div>
+            </el-card>
         </div>
     </div>
 
@@ -57,6 +58,17 @@
 import type { TableItem } from '@/interface/TableItem';
 import { getCurrentInstance, ref, onMounted } from 'vue'
 
+// 按需引入 ECharts 核心模块
+import * as echarts from 'echarts/core';
+// 引入柱状图类型
+import { BarChart, LineChart } from 'echarts/charts';
+// 引入必要的组件（包括缺失的 xAxis、yAxis 和 Grid 组件）
+import { TitleComponent, TooltipComponent, GridComponent, ToolboxComponent, LegendComponent } from 'echarts/components';
+// 引入渲染器
+import { CanvasRenderer } from 'echarts/renderers';
+
+// 注册所有需要使用的图表、组件和渲染器
+echarts.use([BarChart, LineChart, TitleComponent, TooltipComponent, ToolboxComponent, LegendComponent, GridComponent, CanvasRenderer]);
 
 
 
@@ -124,6 +136,94 @@ onMounted(() => {
     getTableData();
     getCountData();
     getChartData();
+
+    // 在组件挂载后初始化 ECharts
+    const myChart = echarts.init(document.getElementById('chart'));
+    // 绘制图表的配置项和数据
+    // const option = {
+    //     title: {
+    //         text: 'ECharts 入门示例'
+    //     },
+    //     tooltip: {},
+    //     xAxis: {
+    //         type: 'category',
+    //         data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+    //     },
+    //     yAxis: {
+    //         type: 'value'
+    //     },
+    //     series: [
+    //         {
+    //             name: '销量',
+    //             type: 'bar',
+    //             data: [5, 20, 36, 10, 10, 20]
+    //         }
+    //     ]
+    // };
+
+    const option = {
+        title: {
+            text: 'Stacked Line'
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        toolbox: {
+            feature: {
+                saveAsImage: {}
+            }
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                name: 'Email',
+                type: 'line',
+                stack: 'Total',
+                data: [120, 132, 101, 134, 90, 230, 210]
+            },
+            {
+                name: 'Union Ads',
+                type: 'line',
+                stack: 'Total',
+                data: [220, 182, 191, 234, 290, 330, 310]
+            },
+            {
+                name: 'Video Ads',
+                type: 'line',
+                stack: 'Total',
+                data: [150, 232, 201, 154, 190, 330, 410]
+            },
+            {
+                name: 'Direct',
+                type: 'line',
+                stack: 'Total',
+                data: [320, 332, 301, 334, 390, 330, 320]
+            },
+            {
+                name: 'Search Engine',
+                type: 'line',
+                stack: 'Total',
+                data: [820, 932, 901, 934, 1290, 1330, 1320]
+            }
+        ]
+    };
+    myChart.setOption(option);
 });
 
 </script>
@@ -225,6 +325,10 @@ onMounted(() => {
             }
         }
     }
+}
+
+:deep(.el-card) {
+    margin-bottom: 20px;
 }
 
 :deep(.el-card__footer) {
