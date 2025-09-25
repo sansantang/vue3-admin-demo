e762433c271c8d5ac31733a33bf2205f29bfabd5
ge.tang <tangge@vip.qq.com>
2025-09-25 14:08:01 +0800

    新增user

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,6 +12,11 @@ const routes = [
         name: 'Home',
         component: () => import('@/views/Home.vue'),
       },
+      {
+        path: '/user',
+        name: 'User',
+        component: () => import('@/views/User.vue'),
+      },
     ],
   },
 ]
---
+++ src/views/User.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="search">
+        <el-button type="primary">新增</el-button>
+        <el-form ref="formRef" :inline="true" :model="numberValidateForm" label-width="auto" class="demo-ruleForm">
+            <el-form-item label="请输入" prop="age">
+                <el-input placeholder="请输入用户名" v-model.number="numberValidateForm.age" type="text" autocomplete="off" />
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="submitForm(formRef)">搜索</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+    <div class="list">
+        <el-table :data="tableData" style="width: 100%">
+            <el-table-column fixed prop="date" label="Date" width="150" />
+            <el-table-column prop="name" label="Name" width="120" />
+            <el-table-column prop="state" label="State" width="120" />
+            <el-table-column prop="city" label="City" width="120" />
+            <el-table-column prop="address" label="Address" width="600" />
+            <el-table-column prop="zip" label="Zip" width="120" />
+            <el-table-column fixed="right" label="Operations" min-width="120">
+                <template #default>
+                    <el-button link type="primary" size="small" @click="handleClick">
+                        编辑
+                    </el-button>
+                    <el-button link type="primary" size="small">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+    </div>
+</template>
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+
+import type { FormInstance } from 'element-plus'
+
+const formRef = ref<FormInstance>()
+
+const numberValidateForm = reactive({
+    age: '',
+})
+
+const submitForm = (formEl: FormInstance | undefined) => {
+    if (!formEl) return
+    formEl.validate((valid) => {
+        if (valid) {
+            console.log('submit!')
+        } else {
+            console.log('error submit!')
+        }
+    })
+}
+
+const handleClick = () => {
+    console.log('click')
+}
+
+const tableData = [
+    {
+        date: '2016-05-03',
+        name: 'Tom',
+        state: 'California',
+        city: 'Los Angeles',
+        address: 'No. 189, Grove St, Los Angeles',
+        zip: 'CA 90036',
+        tag: 'Home',
+    },
+    {
+        date: '2016-05-02',
+        name: 'Tom',
+        state: 'California',
+        city: 'Los Angeles',
+        address: 'No. 189, Grove St, Los Angeles',
+        zip: 'CA 90036',
+        tag: 'Office',
+    },
+    {
+        date: '2016-05-04',
+        name: 'Tom',
+        state: 'California',
+        city: 'Los Angeles',
+        address: 'No. 189, Grove St, Los Angeles',
+        zip: 'CA 90036',
+        tag: 'Home',
+    },
+    {
+        date: '2016-05-01',
+        name: 'Tom',
+        state: 'California',
+        city: 'Los Angeles',
+        address: 'No. 189, Grove St, Los Angeles',
+        zip: 'CA 90036',
+        tag: 'Office',
+    },
+]
+</script>
+<style scoped>
+.search {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
