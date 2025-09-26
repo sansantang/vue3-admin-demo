14117ec4b0f045653a1c092c5d7a0d40c0c91a3b
ge.tang <tangge@vip.qq.com>
2025-09-26 19:25:39 +0800

    login

--- src/router/index.ts
+++ src/router/index.ts
@@ -29,6 +29,11 @@ const routes = [
       // },
     ],
   },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/Login.vue'),
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/Login.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { reactive } from 'vue'
+const loginForm = reactive({
+  username: '',
+  password: ''
+})
+
+function handleLogin() {
+  console.log(loginForm)
+}
+</script>
+
+<template>
+  <div class="body-login">
+    <el-form :model="loginForm" class="login-container">
+      <h1>欢迎登录</h1>
+      <el-form-item>
+        <el-input type="input" placeholder="请输入账号" v-model="loginForm.username"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input type="password" placeholder="请输入密码" v-model="loginForm.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="handleLogin">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<style scoped>
+.body-login {
+  width: 100%;
+  height: 100%;
+  background-size: 100%;
+  overflow: hidden;
+}
+
+.login-container {
+  width: 350px;
+  background-color: #fff;
+  border: 1px solid #eaeaea;
+  border-radius: 15px;
+  padding: 35px 35px 15px 35px;
+  box-shadow: 0 0 25px #cacaca;
+  margin: 250px auto;
+
+  h1 {
+    text-align: center;
+    margin-bottom: 20px;
+    color: #505450;
+  }
+
+  :deep(.el-form-item__content) {
+    justify-content: center;
+  }
+}
+</style>
